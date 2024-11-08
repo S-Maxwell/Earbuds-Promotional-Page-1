3ac2cab3cf1f689468c16f0e507b76152d2a7dd4
S-Maxwell <156356305+S-Maxwell@users.noreply.github.com>
2024-11-08 13:29:10 -0500

    Update Folder Setup
    In this commit, the folders were created and included the required information.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(() => {
+   
+    //create an array, arrays have a 0 based index
+     
+                       //0        1        2        3
+    const students = ["Fred", "Jerome", "Aaron", "Tina"]; 
+
+    console.log(students[0]);
+})();
